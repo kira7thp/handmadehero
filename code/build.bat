51dc4ddd11c788547a29715037c3b7405ccc6ea5
kira7thp <hp4000tals@gmail.com>
2025-06-28 04:32:54 +0200

    meow

---
+++ code/build.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+mkdir ../../build
+pushd ../../build
+cl handmade/code/win32_handmade.cpp
+
+popd
