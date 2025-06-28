299e508d33748da733ea65988865f61ba728f3be
kira7thp <hp4000tals@gmail.com>
2025-06-28 04:45:32 +0200

    a

--- code/build.bat
+++ code/build.bat
@@ -2,8 +2,7 @@
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-mkdir ../../build
-pushd ../../build
-cl handmade/code/win32_handmade.cpp
-
+mkdir ..\..\build
+pushd ..\..\build
+cl -Zi ..\handmadehero\code\win32_handmade.cpp
 popd
